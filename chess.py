3aef3c544c19afc0b86ab751f84428b3003dae44
lauralardies <lrodrlar@myuax.com>
2021-12-12 12:14:52 +0100

    final

--- chess.py
+++ chess.py
@@ -1,15 +1,17 @@
-class ChessBoard():
+class Chess():
         board = []
+        movement = 0
+        board_name = ""
 
-        def __init__(self) -> None:
-                self.board = [['  ' for i in range(8)] for i in range(8)]
-
+        def __init__(self, board_name) -> None:
+                self.board_name = board_name
+                self.board = [[" " for i in range(8)] for i in range(8)]
                 self.chessPieces()
 
-                f = open("board.txt", "w", encoding="utf8")
+                f = open( self.board_name + ".txt", "w", encoding="utf8")
                 for i in range(8):
                         for j in range(8):
-                                f.write(self.board[i][j])
+                                f.write(self.board[i][j] + "\t")
                         f.write("\n")
 
                 f.close
@@ -49,18 +51,88 @@ class ChessBoard():
                 self.board[7][7] = chr(0x265C)
 
         def saveBoard(self):
-                f = open("board.txt", "a", encoding="utf8")
+                f = open(self.board_name + ".txt", "a", encoding="utf8")
                 for i in range(8):
                         for j in range(8):
-                                f.write(self.board[i][j])
+                                f.write(self.board[i][j] + "\t")
                         f.write("\n")
 
                 f.close
 
-        def printBoard(self):
-                f = open("board.txt", "r", encoding="utf8")
-                for line in f:        
-                        print(line[0] + "\t" + line[1] + "\t" + line[2] + "\t" + line[3] + "\t" + line[4] + "\t" + line[5] + "\t" + line[6] + "\t" + line[7])
+        def printBoard(self, movement):
+                i = (movement - 1) * 8
+                j = movement * 8 - 1
 
-chessBoard = ChessBoard()
-chessBoard.printBoard()
+                f = open(self.board_name + ".txt", "r", encoding="utf8")
+                lines = f.readlines()
+
+                for k in range(i, j + 1):
+                        print(lines[k])
+
+        def validPlacement(self, x, y, placement):
+                if x in range(8):
+                        if y in range (8):
+                                if placement == True:
+                                        if self.board[x][y] == " ":
+                                                print("There is not a piece here! Please select a row and a column in which there is a piece.")
+                                                return False
+                                        return True
+                                if placement == False:
+                                        return True
+                        else:
+                                print("The column you've introduced is not on the board! Please, start again.")
+                                return False
+                else:
+                        print("The row you've introduced is not on the board! Please, start again.")
+                        return False
+
+        def movePiece(self):
+                while True:
+                        decision = input("Do you want to make a move? [Y]/N: ")
+                        decision = decision.capitalize()
+
+                        if decision == "Y":
+                                piece_row = int(input("Which row is the piece you want to move in?: (Please select a row from 0 to 7)\n"))
+                                if self.validPlacement(piece_row, 0, False) == True:
+                                        piece_column = int(input("Which column is the piece you want to move in?: (Please select a column from 0 to 7)\n"))
+                                        if self.validPlacement(piece_row, piece_column, True) == True:
+                                                piece = self.board[piece_row][piece_column]
+                                                self.board[piece_row][piece_column] = " "
+                                                final_row = int(input("Which row do you want to move your piece into?: (Please select a row from 0 to 7)\n"))
+                                                if self.validPlacement(final_row, 0, False) == True:
+                                                        final_column = int(input("Which column do you want to move your piece into?: (Please select a column from 0 to 7)\n"))
+                                                        if self.validPlacement(final_row, final_column, False) == True:
+                                                                self.board[final_row][final_column] = piece
+                                                                self.saveBoard()
+                                                                self.movement = self.movement + 1
+                                                                self.printBoard(self.movement + 1)
+
+                        if decision == "N":
+                                if self.movement != 0:
+                                        while True:
+                                                question = input("Do you want to review any movements of the game? [Y]/N: ")
+                                                question = question.capitalize()
+
+                                                if self.movement > 1:
+                                                        num_mov = "movements"
+                                                else:
+                                                        num_mov = "movement"
+
+                                                if question == "Y":
+                                                        review = int(input("Which movement do you want to review? (Please bear in mind that you've done " + str(self.movement) + " " + str(num_mov) + "): "))
+                                                        if review in range(self.movement + 1):
+                                                                self.printBoard(review + 1)
+                                                        else:
+                                                                print("There isn't a movement like that! Please check that you haven't introduced a number higher than " + str(self.movement) + ".")
+                                                if question == "N":
+                                                        print("Thank you for playing chess! See you later!")
+                                                        break
+                                        break
+                                else:
+                                        print("Thank you for playing chess! See you later!")
+                                        break
+
+file_name = input("What name do you want to give to your chess game?: ")
+chess_game = Chess(file_name)
+chess_game.printBoard(1)
+chess_game.movePiece()
